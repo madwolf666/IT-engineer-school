6ba8c746943be61f0e3a02f194cae553f35fbc21
Hitohi Nishimura <madwolf666@live.jp>
2017-06-04 01:00:10 +0900

    新規

---
+++ public_html/observe-network/GetEventLog.ps1
@@ -0,0 +1,25 @@
+Write-Host "End to [Ctrl + C]"
+
+$EventLog = "EventLogSink"
+Register-WmiEvent -Query "SELECT * FROM InstanceCreationEvent WHERE TargetInstance ISA 'Win32_NTLogEvent'" -SourceIdentifier $EventLog
+
+try{
+    While ($True) {
+        $NewEvent = Wait-Event -SourceIdentifier $EventLog
+        $Log = $NewEvent.SourceEventArgs.NewEvent.TargetInstance
+        $LogName = $Log.LogFile; $SourceName = $Log.SourceName
+        $EventCode = $Log.EventCode; $TimeGenerated = $Log.TimeGenerated
+        $Year = $TimeGenerated.SubString(0, 4); $Month = $TimeGenerated.SubString(4, 2)
+        $Day = $TimeGenerated.SubString(6, 2); $Hour = $TimeGenerated.SubString(8, 2)
+        $Minutes = $TimeGenerated.SubString(10, 42)
+        $Date = $Year + "/" + $Month + "/" + $Day + " " + $Hour + ":" + $Minutes
+        $Date = (([DateTime]$Date)).AddHours(9).ToString("yyyy/MM/dd HH:mm:ss")
+        $Message = $Log.Mesage
+    }
+}Catch{
+    Write-Warning "Error"
+    $Error[0]
+}Finally{
+    Get-Event | Remove-Event
+    Get-EventSubscriber | Unregister-Event
+}
